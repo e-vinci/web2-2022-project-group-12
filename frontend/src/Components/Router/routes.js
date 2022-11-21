be480b41269adeed97a534f725487ae33ce9a372
Victor Morabito <victor.morabito@student.vinci.be>
2022-11-21 11:18:48 +0100

    Added Register page (blank)

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,10 +1,12 @@
 
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import RegisterPage from '../Pages/RegisterPage';
 
 const routes = {
   '/': HomePage,
   '/new': NewPage,
+  '/register' : RegisterPage,
 };
 
 export default routes;
