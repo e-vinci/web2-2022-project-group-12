d4a16b3447518bace2cad4b4b1fa12d0b93ae0dd
Victor Morabito <victor.morabito@student.vinci.be>
2022-12-07 12:12:05 +0100

    Cart phase 2 : items add to cart/ no display yet

--- frontend/src/Components/Pages/MyCartPage.js
+++ frontend/src/Components/Pages/MyCartPage.js
@@ -1,32 +1,50 @@
+import { Item, loadCart, saveCart } from '../../utils/utilsCart';
 
 const MyCartPage = () => {
+  // si la session contient un panier, afficher un bouton pour le supprimer (Ceci sert uniquepent de test de localStorage)
+  if (sessionStorage.getItem('shoppingCart') != null) {
+    const main = document.querySelector('main');
+    main.innerHTML = `<button type="button" id="removeButton"class="btn btn-primary">Supprimer Panier</button>`;
+  } else {
+    // si non (ou quand on clique sur supprimer panier, affiche qu'il n'y a pas de panier)
+    const main = document.querySelector('main');
+    main.innerHTML = "Il n'y a pas de panier ";
+  }
+  // supprimer le panier
+  const deleteCartTest = () => {
+    sessionStorage.removeItem('shoppingCart');
+    MyCartPage();
+  };
 
-    // si la session contient un panier, afficher un bouton pour le supprimer (Ceci sert uniquepent de test de localStorage)
-    if (sessionStorage.getItem("shoppingCart") != null) {
-        const main = document.querySelector('main');
-        main.innerHTML = `<button type="button" id="removeButton"class="btn btn-primary">Supprimer Panier</button>`;
-        
-    }else{
-    // si non (ou quand on clique sur supprimer panier, affiche qu'il n'y a pas de panier)    
-        const main = document.querySelector('main');
-        main.innerHTML = "Il n'y a pas de panier ";
+  const btn = document.getElementById('removeButton');
+  btn.addEventListener('click', deleteCartTest);
+
+  let cart = loadCart();
+
+  
+  function addItemToCart(name, price, count) {
+    cart = loadCart();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count += count;
+        saveCart(cart);
+        return;
+      }
     }
-    // supprimer le panier
-    const  deleteCartTest =()=>{
-        sessionStorage.removeItem('shoppingCart');
-        MyCartPage();
-    }
-    
+    const itemToadd = new Item(name, price, count);
+    cart.push(itemToadd);
+    saveCart(cart);
+  };
 
-    const btn = document.getElementById('removeButton');
-    btn.addEventListener('click', deleteCartTest);
+  addItemToCart("pomme",5,5);
+  // eslint-disable-next-line no-console
+  console.log("LE PAINIER EST", cart);
+  addItemToCart("pomme",5,5);
+  addItemToCart("orange",5,5);
+// eslint-disable-next-line no-console
+  console.log("LE PAINIER EST", cart);
 
-    
-    
-    /*  function loadCart() {
-        const cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
-        return cart;
-    } */
 
-}
-    export default MyCartPage;
+};
+export default MyCartPage;
--- frontend/src/utils/utilsCart.js
+++ frontend/src/utils/utilsCart.js
@@ -1,18 +1,29 @@
 const shoppingCart = () => {
-    const cart = [];
-    saveCart();
-    function saveCart() {
-        sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
-    }
+  const cart = [];
+
+  saveCart(cart);
+};
+function saveCart(cart) {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+  saveCart();
+
+
+function loadCart() {
+  const cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  return cart;
 }
 
-   function loadCart() {
-        const cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
-        return cart;
-    } 
+function deleteCart() {
+  sessionStorage.removeItem('shoppingCart');
+}
 
-    function deleteCart() {
-        sessionStorage.removeItem('shoppingCart');
-    }
 
-export { shoppingCart, loadCart, deleteCart }
+
+function Item(name, price, count) {
+  this.name = name;
+  this.price = price;
+  this.count = count;
+}
+
+export { shoppingCart, loadCart, deleteCart,saveCart , Item };
