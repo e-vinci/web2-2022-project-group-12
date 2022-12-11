a0273ba5c9d33e4a95768079d5e49a0b754e1b49
Mykhailo Kretsu <mykhailo.kretsu@student.vinci.be>
2022-12-11 21:17:30 +0100

    Ajouts de commentaires au code

--- api/routes/users.js
+++ api/routes/users.js
@@ -13,13 +13,14 @@ router.get('/', (req, res) => {
   res.json({ users: [{ name: 'e-baron' }] });
 });
 
-// inscription
+// Enregistrement d'un nouveau utilisateur dans la base des données
 router.post('/register', async (req,res)=>{
   console.log(req.body)
   const user = await userModel.addUser(req.body);
   return res.json(user);
 });
 
+// Routeur permettant de se connecter comme utilisateur //
 router.post('/login', async(req,res)=>{
   console.log(req.body);
   const {email} = req.body;
@@ -33,10 +34,17 @@ router.post('/login', async(req,res)=>{
   return res.json(logedInUser);
 });
 
-// Permet de récupérer un produit en particulier avec id
+// Permet de récupérer un vendeur en particulier avec id
 router.get('/getIdStore/:id' , async(req,res)=>{
-  const result = await userModel.getOneUser(req.params.id);
+  const result = await userModel.getSeller(req.params.id);
   res.send(result);
+});
+
+// Permet au utilisateur de devenir vendeur//
+router.post('/becomeSeller', async(req, res)=>{
+  console.log(req.body);
+  const store = await userModel.beSeller(req.body);
+  return res.json(store);
 });
 
 module.exports = router;
